9627cd023031f09bf71d6f8c3aa83e7ac09a56d1
Mani Bharathi <manibharath79@gmail.com>
2019-09-19 21:54:36 +0530

    UI screen designed

---
+++ js/todo.js
@@ -0,0 +1,46 @@
+function getElementById(id) {
+    return document.getElementById(id);
+}
+
+function getElementsByClass(className) {
+    return document.getElementsByClassName(className);
+}
+
+function getElementValueById(id) {
+    return document.getElementById(id).value;
+}
+
+function setElementValueById(id, value) {
+    document.getElementById(id).value = value;
+}
+
+function openMenu() {
+    var iconValue = getElementValueById("icon");
+    if (iconValue == "close") {
+        var sideBar = getElementById("side-nav-bar");
+        sideBar.style.width= "20%";
+        var sideBarDesc = getElementsByClass("icon-description");
+        sideBarDesc[0].style.display="block";
+        sideBarDesc[1].style.display="block";
+        sideBarDesc[2].style.display="block";
+        var taskMenu = getElementsByClass("action-description");
+        taskMenu[0].style.display="block";
+        var addList = getElementsByClass("new-list");
+        addList[0].style.display="block";
+        setElementValueById("icon", "open");
+   }
+
+    if (iconValue == "open") {
+        var sideBar = getElementById("side-nav-bar");
+        sideBar.style.width= "0%";
+        var sideBarDesc = getElementsByClass("icon-description");
+        sideBarDesc[0].style.display="none";
+        sideBarDesc[1].style.display="none";
+        sideBarDesc[2].style.display="none";
+        var taskMenu = getElementsByClass("action-description");
+        taskMenu[0].style.display="none";
+        var addList = getElementsByClass("new-list");
+        addList[0].style.display="none";
+        setElementValueById("icon", "close");
+   }
+}

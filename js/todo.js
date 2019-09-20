315fea5e5c9f4c383e6c867282bfbc370b65a0fb
Mani Bharathi <manibharath79@gmail.com>
2019-09-20 16:35:20 +0530

    right  bar created

--- js/todo.js
+++ js/todo.js
@@ -20,33 +20,35 @@ menuBtn.addEventListener("click", openMenu);
 var newList = getElementById("new-list");
 newList.addEventListener("click", openMenu);
 
+var minimize = getElementById("minimize");
+minimize.addEventListener("click", minimizeInfo);
+
+var info = getElementById("info");
+info.addEventListener("click", getInfo);
+
 function openMenu() {
     var iconValue = getElementValueById("icon");
+    var sideBar = getElementById("side-nav-bar");
     if (iconValue == "close") {
-        var sideBar = getElementById("side-nav-bar");
-        sideBar.style.width= "20%";
-        var sideBarDesc = getElementsByClass("icon-description");
-        sideBarDesc[0].style.display="block";
-        sideBarDesc[1].style.display="block";
-        sideBarDesc[2].style.display="block";
-        var taskMenu = getElementsByClass("action-description");
-        taskMenu[0].style.display="block";
-        var addList = getElementsByClass("new-list");
-        addList[0].style.display="block";
+        sideBar.style.display= "block";
         setElementValueById("icon", "open");
-   }
-
+    }
     if (iconValue == "open") {
-        var sideBar = getElementById("side-nav-bar");
-        sideBar.style.width= "0%";
-        var sideBarDesc = getElementsByClass("icon-description");
-        sideBarDesc[0].style.display="none";
-        sideBarDesc[1].style.display="none";
-        sideBarDesc[2].style.display="none";
-        var taskMenu = getElementsByClass("action-description");
-        taskMenu[0].style.display="none";
-        var addList = getElementsByClass("new-list");
-        addList[0].style.display="none";
+        sideBar.style.display= "none";
         setElementValueById("icon", "close");
-   }
+    }
+}
+
+function minimizeInfo() {
+   var taskInfo = getElementsByClass("task-info");
+   taskInfo[0].style.display="none";
+   var sideBar = getElementById("side-nav-bar");
+   sideBar.style.width="23.8%";
+}
+
+function getInfo() {
+   var taskInfo = getElementsByClass("task-info");
+   var sideBar = getElementById("side-nav-bar");
+   sideBar.style.width="32.8%";
+   taskInfo[0].style.display="block";
 }

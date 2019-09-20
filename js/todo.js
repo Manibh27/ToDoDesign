6fe8904d9a598df3c86553d5d9885f8243ac8b88
Mani Bharathi <manibharath79@gmail.com>
2019-09-20 21:34:50 +0530

    Creating new task dynamically

--- js/todo.js
+++ js/todo.js
@@ -6,6 +6,23 @@ function getElementsByClass(className) {
     return document.getElementsByClassName(className);
 }
 
+function getDivWithClass(className) {
+    var newTask = document.createElement("DIV");
+    newTask.setAttribute("class", className);     
+    return newTask;
+}
+
+function getTextInputWithClass(className) {
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute("class", className); 
+    return input;
+}
+
+function getNewDiv() {
+    return document.createElement("DIV");
+}
+
 function getElementValueById(id) {
     return document.getElementById(id).value;
 }
@@ -52,3 +69,21 @@ function getInfo() {
    sideBar.style.width="32.8%";
    taskInfo[0].style.display="block";
 }
+
+var addTask = getElementsByClass("add-task-input");
+console.log(addTask.length - 1);
+addTask[addTask.length - 1].addEventListener("keyup", function createNewTaskDiv(e) {
+    if (e.keyCode === 13) {
+        console.log(addTask.length - 1);
+        var newTask = getDivWithClass("add-new-task");
+        addTask[addTask.length - 1].setAttribute("id", "");
+        var textInput = getTextInputWithClass("add-task-input");
+        textInput.setAttribute("id", "add-new-task");
+        newTask.innerHTML =
+            '<a href="#add-new-task"><i class="material-icons new-task-icon">add</i></a>';
+        newTask.appendChild(textInput);
+        var todoListDiv = getElementById("todo-list");
+        todoListDiv.appendChild(newTask);
+        addTask = getElementsByClass("add-task-input");
+    }
+});

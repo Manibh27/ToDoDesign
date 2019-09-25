ff8f0afaefc6de43c2bed7928b4bf26f441e8112
Mani Bharathi <manibharath79@gmail.com>
2019-09-25 19:14:03 +0530

    Developing delete option

--- js/todo.js
+++ js/todo.js
@@ -163,20 +163,28 @@ function getInfo() {
    if (lists[id].tasks[taskId].isComplete === false) {
        getElementsByClass("task-info-input")[0].style.textDecoration = "line-through";
    }
+   
+   getElementsByClass("notes")[0].innerHTML = lists[id].tasks[taskId].notes;
    getElementById("steps").innerHTML = "";
    var stepInfos = lists[id].tasks[taskId].steps;
-   for (var step = 0 ; step < stepInfos.length; step = step + 1) {
-        var newStep = getDivWithClass("created-steps");
-        var steps = getElementById("steps");
-        newStep.innerHTML = "<a class='step-icon-link'>"
+    for (var step = 0 ; step < stepInfos.length; step = step + 1) {
+        if (stepInfos[step].deleteStatus !== true) {
+            var newStep = getDivWithClass("created-steps");
+            var steps = getElementById("steps");
+            newStep.innerHTML = "<a class='step-icon-link'>"
                 + "<i class='material-icons step-icon'>check_circle_outline</i></a>"+
-                "<p class='step'>"+stepInfos[step].name+"</p>";
-        steps.appendChild(newStep);
-        if (stepInfos[step].isComplete !== true) {
-            getElementsByClass("step")[step].style.textDecoration = "line-through";
-            getElementsByClass("step-icon")[step].innerHTML = "check_circle";
+                "<p class='step'>"+stepInfos[step].name+"</p> <p class='delete-step'>x</p>";
+            steps.appendChild(newStep);
+            if (stepInfos[step].isComplete !== true) {
+                getElementsByClass("step")[step].style.textDecoration = "line-through";
+                getElementsByClass("step-icon")[step].innerHTML = "check_circle";
+            }
+            getElementsByClass("step-icon")[step].addEventListener("click", finishStep.bind(stepInfos[step]));
+            var deleteStep = getElementsByClass("delete-step");
+            deleteStep[deleteStep.length - 1].addEventListener("click", deleteSubTask.bind(stepInfos[step]));
+            newStep.addEventListener("mouseover", viewDeleteIcon.bind(deleteStep.length - 1));
+            newStep.addEventListener("mouseout", hideDeleteIcon.bind(deleteStep.length - 1));
         }
-        getElementsByClass("step-icon")[step].addEventListener("click", finishStep.bind(stepInfos[step]));
    }
    getElementById("add-step").value = "";
 }
@@ -204,9 +212,11 @@ function createNewTaskDiv(event) {
         tasks = lists[id].tasks;
         task.id = tasks.length;
         taskId = task.id;
+        task.deleteStatus = false;
         task.isComplete = true;
         task.name = event.target.value;
         task.steps = [];
+        task.notes;
         tasks.push(task);
 
 
@@ -402,29 +412,31 @@ function changeList() {
     var existingDiv = getElementsByClass('tick-icon');
     var existingDivIcon = getElementById('task-container').innerHTML = "";
     for(var task = 0; task < this.tasks.length; task = task + 1) {
-        var taskContainer = getElementById("task-container");     
-        var newDiv = getDivWithClass("added-task");	
-        taskId = this.tasks[task].id;
-        var stepLength = getStepsLength();
+        console.log(this.tasks[task].deleteStatus);
+        if (this.tasks[task].deleteStatus !== true) {
+            var taskContainer = getElementById("task-container");     
+            var newDiv = getDivWithClass("added-task");	
+            taskId = this.tasks[task].id;
+            var stepLength = getStepsLength();
 
-        // After new div is created the previous div is modified. 
-        newDiv.innerHTML = 
-            "<a><i class='material-icons tick-icon'>check_circle_outline</i></a>"
-             +"<p class='task-name'>"+this.tasks[task].name+"</p>" +
-             "<p class='step-count'>"+stepLength+ " of " + lists[id].tasks[taskId].steps.length+"</p>";
-        taskContainer.appendChild(newDiv);
-        var completeIcon = getElementsByClass("tick-icon");
-        var taskDetails = getElementsByClass("task-name");
-        taskDetails[task].addEventListener("click", getInfo.bind(this.tasks[task]));
-        completeIcon[task].addEventListener("focus", showCompleteIcon);
-        if (this.tasks[task].isComplete === false) {
-            taskDetails[task].style.textDecoration = "line-through";
-            getElementsByClass("tick-icon")[task].innerHTML = "check_circle";
-        } 
-        completeIcon[task].addEventListener("click", finishTask.bind(task));
-        getElementById("add-new-task").value = "";
+            // After new div is created the previous div is modified. 
+            newDiv.innerHTML = 
+                "<a><i class='material-icons tick-icon'>check_circle_outline</i></a>"
+                +"<p class='task-name'>"+this.tasks[task].name+"</p>" +
+                "<p class='step-count'>"+stepLength+ " of " + lists[id].tasks[taskId].steps.length+"</p>";
+            taskContainer.appendChild(newDiv);
+            var completeIcon = getElementsByClass("tick-icon");
+            var taskDetails = getElementsByClass("task-name");
+            taskDetails[task].addEventListener("click", getInfo.bind(this.tasks[task]));
+            completeIcon[task].addEventListener("focus", showCompleteIcon);
+            if (this.tasks[task].isComplete === false) {
+                taskDetails[task].style.textDecoration = "line-through";
+                getElementsByClass("tick-icon")[task].innerHTML = "check_circle";
+            } 
+            completeIcon[task].addEventListener("click", finishTask.bind(task));
+            getElementById("add-new-task").value = "";
+        }
     }
-    console.log(this.tasks.length);
     if (this.tasks.length > 4) {
         taskContainer.style.height = "400px";
         taskContainer.style.overflow = "auto";
@@ -446,10 +458,11 @@ function addStep(event) {
         var steps = getElementById("steps");
         newStep.innerHTML = "<a class='step-icon-link'>"
             + "<i class='material-icons step-icon'>check_circle_outline</i></a>"+
-            "<p class='step'>"+event.target.value+"</p>";
+            "<p class='step'>"+event.target.value+"</p> <p class='delete-step'>x</p>";
         steps.style.height = "";
         steps.appendChild(newStep);
         var step = {};
+        step.deleteStatus = false;
         step.name = event.target.value;
         step.id = lists[id].tasks[taskId].steps.length;
         step.isComplete = true;
@@ -462,9 +475,29 @@ function addStep(event) {
         }
         getElementsByClass("step-count")[taskId].innerHTML = 
                   getStepsLength() + " of " + lists[id].tasks[taskId].steps.length;
+        var deleteStep = getElementsByClass("delete-step");
+        deleteStep[deleteStep.length - 1].addEventListener("click", deleteSubTask.bind(step));
+        newStep.addEventListener("mouseover", viewDeleteIcon.bind(deleteStep.length - 1));
+        newStep.addEventListener("mouseout", hideDeleteIcon.bind(deleteStep.length - 1));
     }
 }
 
+function viewDeleteIcon() {
+    getElementsByClass("delete-step")[this].style.display = "block";
+}
+
+function hideDeleteIcon() {
+    getElementsByClass("delete-step")[this].style.display = "none";
+}
+
+function deleteSubTask() {
+    this.isComplete = false;
+    this.deleteStatus = true;
+    getElementsByClass("step-count")[taskId].innerHTML = 
+            getStepsLength() + " of " + lists[id].tasks[taskId].steps.length;
+    var updateSteps = getInfo.bind(lists[id].tasks[taskId]);
+    updateSteps();
+}
 /**
  * Return the length of sub tasks which is not completed of the current task.
  * 
@@ -497,3 +530,32 @@ function finishStep() {
 var homeList = getElementsByClass("task-desc");
 homeList[0].addEventListener("click", changeList.bind(lists[0]));
 
+var notes = getElementsByClass("notes");
+notes[0].addEventListener("blur", saveNotes);
+
+function saveNotes(event) {
+    lists[id].tasks[taskId].notes = event.target.textContent;
+}
+
+var deleteIcon = getElementById("delete");
+deleteIcon.addEventListener("click", getConfirmation);
+
+function getConfirmation() {
+    var modal = document.getElementById("myModal");
+    var cancel = getElementsByClass("cancel")[0];
+    modal.style.display = "block";
+    cancel.onclick = function() {
+        modal.style.display = "none";
+    }
+    var deleteTask = getElementsByClass("delete-task")[0];
+    deleteTask.onclick = function() {
+        modal.style.display = "none";
+        console.log(lists[id]);
+        lists[id].tasks[taskId].isComplete = false;
+        lists[id].tasks[taskId].deleteStatus = true;
+        var updateTasks = changeList.bind(lists[id]);
+        updateTasks();
+        minimizeInfo();
+        getElementsByClass("tasks-count")[id - 1].innerHTML = getTasksLength();
+    }
+}
